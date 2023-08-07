1940a73a90bc4f5fe8b26bc83fb11568c9e9f081
bitnaleeeee <bitnaleeeee@gmail.com>
2023-08-07 21:08:01 +0900

    map함수

--- src/components/Line.jsx
+++ src/components/Line.jsx
@@ -25,119 +25,36 @@ const Line = () => {
             </a>
           </h2>
           <ul className="list">
-            <li>
-              <a href="#!">
-                <div className="thumbnail">
-                  <span className="clip new">NEW</span>
-                  <img
-                    src={data[0] && data[0].list[0].imgSrc}
-                    alt=""
-                    className="img"
-                  />{" "}
-                  <div className="bookmark">
-                    <input
-                      type="checkbox"
-                      className="bookmark"
-                      id="check-01-01"
-                      // onChange={checkedBox}
-                      // checked={isChecked}
-                    />
-                    <label htmlFor="check-01-01" />
-                  </div>
-                </div>
-                <p className="info">
-                  <span className="point">
-                    {data[0] && data[0].list[0].subTitle}
-                  </span>
-                  {data[0] && data[0].list[0].info}{" "}
-                </p>
-              </a>
-            </li>
-            <li>
-              <a href="#!">
-                <div className="thumbnail">
-                  <span className="clip new">NEW</span>
-                  <img
-                    src={data[0] && data[0].list[1].imgSrc}
-                    alt=""
-                    className="img"
-                  />
-                  <div className="bookmark">
-                    <input
-                      type="checkbox"
-                      className="bookmark"
-                      id="check-01-02"
-                      // onChange={checkedBox}
-                    />
-                    <label htmlFor="check-01-02" />
-                  </div>
-                </div>
-                <p className="info">
-                  <span className="point">
-                    {" "}
-                    {data[0] && data[0].list[1].subTitle}
-                  </span>
-                  {data[0] && data[0].list[1].info}{" "}
-                </p>
-              </a>
-            </li>
-            <li>
-              <a href="#!">
-                <div className="thumbnail">
-                  <img
-                    src={data[0] && data[0].list[2].imgSrc}
-                    alt=""
-                    className="img"
-                  />
-                  <div className="bookmark">
-                    <input
-                      type="checkbox"
-                      className="bookmark"
-                      id="check-01-03"
-                      // onChange={checkedBox}
-                    />
-                    <label htmlFor="check-01-03" />
-                  </div>
-                </div>
-                <p className="info">
-                  <span className="point">
-                    {" "}
-                    {data[0] && data[0].list[2].subTitle}
-                  </span>
-                  {data[0] && data[0].list[2].info}
-                </p>
-              </a>
-            </li>
-            <li>
-              <a href="#!">
-                <div className="thumbnail">
-                  <img
-                    src={data[0] && data[0].list[3].imgSrc}
-                    alt=""
-                    className="img"
-                  />
-                  <div className="bookmark">
-                    <input
-                      type="checkbox"
-                      className="bookmark"
-                      id="check-01-04"
-                      // onChange={checkedBox}
-                    />
-                    <label htmlFor="check-01-04" />
-                  </div>
-                </div>
-                <p className="info">
-                  <span className="point">
-                    {" "}
-                    {data[0] && data[0].list[3].subTitle}
-                  </span>
-                  {data[0] && data[0].list[3].info}
-                </p>
-              </a>
-            </li>
+            {data.length &&
+              data[0].list.map((item, idx) => {
+                return (
+                  <li key={idx}>
+                    <a href="#!">
+                      <div className="thumbnail">
+                        <span className="clip new">NEW</span>
+                        <img src={item.imgSrc} alt="" className="img" />{" "}
+                        <div className="bookmark">
+                          <input
+                            type="checkbox"
+                            className="bookmark"
+                            id="check-01-01"
+                            defaultChecked={item.bookMark}
+                          />
+                          <label htmlFor="check-01-01" />
+                        </div>
+                      </div>
+                      <p className="info">
+                        <span className="point">{item.subTitle}</span>
+                        {item.info}{" "}
+                      </p>
+                    </a>
+                  </li>
+                );
+              })}
           </ul>
         </div>
       </div>
+
       <hr className="line" />
       <div className="inner">
         <div className="thumbnail-list">
@@ -148,116 +65,31 @@ const Line = () => {
             </a>
           </h2>
           <ul className="list">
-            <li>
-              <a href="#!">
-                <div className="thumbnail">
-                  <img
-                    src={data[1] && data[1].list[0].imgSrc}
-                    alt=""
-                    className="img"
-                  />
-                  <div className="bookmark">
-                    <input
-                      type="checkbox"
-                      className="bookmark"
-                      id="check-02-01"
-                      // onChange={checkedBox}
-                    />
-                    <label htmlFor="check-02-01" />
-                  </div>
-                </div>
-                <p className="info">
-                  <span className="point">
-                    {" "}
-                    {data[1] && data[1].list[0].subTitle}
-                  </span>{" "}
-                  {data[1] && data[1].list[0].info}
-                </p>
-              </a>
-            </li>
-            <li>
-              <a href="#!">
-                <div className="thumbnail">
-                  <span className="clip new">NEW</span>
-                  <img
-                    src={data[1] && data[1].list[1].imgSrc}
-                    alt=""
-                    className="img"
-                  />
-                  <div className="bookmark">
-                    <input
-                      type="checkbox"
-                      className="bookmark"
-                      id="check-02-02"
-                      // onChange={checkedBox}
-                    />
-                    <label htmlFor="check-02-02" />
-                  </div>
-                </div>
-                <p className="info">
-                  <span className="point">
-                    {" "}
-                    {data[1] && data[1].list[1].subTitle}
-                  </span>
-                  {data[1] && data[1].list[1].info}
-                </p>
-              </a>
-            </li>
-            <li>
-              <a href="#!">
-                <div className="thumbnail">
-                  <span className="clip new">NEW</span>
-                  <img
-                    src={data[1] && data[1].list[2].imgSrc}
-                    alt=""
-                    className="img"
-                  />
-                  <div className="bookmark">
-                    <input
-                      type="checkbox"
-                      className="bookmark"
-                      id="check-02-03"
-                      // onChange={checkedBox}
-                    />
-                    <label htmlFor="check-02-03" />
-                  </div>
-                </div>
-                <p className="info">
-                  <span className="point">
-                    {" "}
-                    {data[1] && data[1].list[2].subTitle}
-                  </span>{" "}
-                  {data[1] && data[1].list[2].info}
-                </p>
-              </a>
-            </li>
-            <li>
-              <a href="#!">
-                <div className="thumbnail">
-                  <img
-                    src={data[1] && data[1].list[3].imgSrc}
-                    alt=""
-                    className="img"
-                  />
-                  <div className="bookmark">
-                    <input
-                      type="checkbox"
-                      className="bookmark"
-                      id="check-02-04"
-                      // onChange={checkedBox}
-                    />
-                    <label htmlFor="check-02-04" />
-                  </div>
-                </div>
-                <p className="info">
-                  <span className="point">
-                    {" "}
-                    {data[1] && data[1].list[3].subTitle}
-                  </span>{" "}
-                  {data[1] && data[1].list[3].info}
-                </p>
-              </a>
-            </li>
+            {data.length &&
+              data[1].list.map((item, idx) => {
+                return (
+                  <li key={idx}>
+                    <a href="#!">
+                      <div className="thumbnail">
+                        <img src={item.imgSrc} alt="" className="img" />
+                        <div className="bookmark">
+                          <input
+                            type="checkbox"
+                            className="bookmark"
+                            id="check-02-01"
+                            defaultChecked={item.bookMark}
+                          />
+                          <label htmlFor="check-02-01" />
+                        </div>
+                      </div>
+                      <p className="info">
+                        <span className="point"> {item.subTitle}</span>{" "}
+                        {item.info}
+                      </p>
+                    </a>
+                  </li>
+                );
+              })}
           </ul>
         </div>
       </div>
